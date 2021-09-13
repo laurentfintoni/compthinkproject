aecc581991c7a0faa74e5086f4f87ba3cafed24a
enri-ca <72966913+enri-ca@users.noreply.github.com>
2021-09-13 23:54:26 +0200

    Update Bibengine_inprogress.py

--- Bibengine_inprogress.py
+++ Bibengine_inprogress.py
@@ -455,77 +455,72 @@ def do_search(data, query, field):
 #FUNCTION 9 DO_FILTER_BY_VALUE (EVERYONE)
 
 def do_filter_by_value(data, query, field):
-    #return error if input query is not a string
+    # create a list for results
+    filter_result = []
+    # print an alert and return initial filter_result if input query is not a string or input field is not a string + catch eroneous field inputs w/ regex
     if type(query) is not str or query == '':
-        return 'The input query must be a string with at least one character \U0001F913.'
-    #return error if input field is not a string + catch eroneous field inputs w/ regex
-    if type(field) is not str or field == '':
-        return 'The chosen field for queries must be a string \U0001F913.'
+        print('The input query must be a string with at least one character \U0001F913.')
+    elif type(field) is not str or field == '':
+        print('The chosen field for queries must be a string with the values citing, cited, creation or timespan \U0001F913.')
     else:
         field_pattern = r'(citing|cited|creation|timespan)'
         field_match = re.fullmatch(field_pattern, field)
         if field_match == None:
-            return 'The field input must be either citing, cited, creation or timespan \U0001F913.'
-    #create a dictionary containing possible comparison operators using operator library
-    c_ops = {'<': operator.lt, '<=': operator.le, '>': operator.gt, '>=': operator.ge, '==': operator.eq, '!=': operator.ne}
-    #create a list for results
-    filter_result = []
-    #lowercase the query
-    l_query = query.lower()
-    #check for operators in query
-    #if not re.search(r'<\s|\s<|>\s|\s>|<=\s|\s<=|>=\s|\s>=|==\s|\s==|!=\s|\s!=|\sand\s|\sor\s|\snot\s', n_query):
-    if not re.search(r'^<\s|^>\s|^<=\s|^>=\s|^==\s|^!=\s|\sand\s|\sor\s|\snot\s', l_query):
-        for row in data:
-            if re.search(l_query, row[field].lower()):
-                filter_result.append(row)
-        if len(filter_result) == 0:
-            return 'There are no results for your query, please try again \U0001F647'
+            print('The field input must be either citing, cited, creation or timespan \U0001F913.')
         else:
-            return filter_result
-    #if there are operators in query
-    else:
-        # split the query into tokens
-        spl_query = l_query.split(" ")
-        #check if the query contains more than three tokens or just one token
-        if 2 < len(spl_query) > 3:
-            return 'Your query must follow the following format: "<operator> <tokens>" for comparisons or "<tokens 1> <operator> <tokens 2>" for boolean searches. There appears to be too many tokens \U0001F645.'
-        #if query has two tokens
-        elif len(spl_query) == 2:
-            #check that first token is a valid operator
-            if not re.fullmatch(r'<|>|<=|>=|==|!=', spl_query[0]) or re.fullmatch(r'<|>|<=|>=|==|!=', spl_query[1]):
-                return 'It looks like your query is not written with the following format: "<operator> <tokens>" for comparison \U0001F645.'
-            else:
-                #look for first token in dictionary of operators and set it as a variable
-                co_ops = c_ops[spl_query[0]]
-                v_query = spl_query[1]
-            #search for query in data using operator library
-            for row in data:
-                if co_ops(row[field].lower(), v_query):
-                    filter_result.append(row)
-            if len(filter_result) == 0:
-                return 'There are no results for your query, please try again \U0001F647'
-            else:
-                return filter_result
-        #if query has three tokens
-        elif len(spl_query) == 3:
-            #check that second token is a boolean
-            if not re.match(r'and|or|not', spl_query[1]):
-                return 'Your query must follow the following format: "<tokens 1> <operator> <tokens 2>." The boolean operator seems to be in the wrong place \U0001F631.'
-            else:
+            # create a dictionary containing possible comparison operators using operator library
+            c_ops = {'<': operator.lt, '<=': operator.le, '>': operator.gt, '>=': operator.ge, '==': operator.eq,
+                     '!=': operator.ne}
+            # lowercase the query
+            l_query = query.lower()
+            # check for operators in query: comparisons operators at the beginning of the string, boolean with space after and before
+            if not re.search(r'^<\s|^>\s|^<=\s|^>=\s|^==\s|^!=\s|\sand\s|\sor\s|\snot\s', l_query):
                 for row in data:
-                    v1_query = re.search(spl_query[0], row[field].lower())
-                    v2_query = re.search(spl_query[2], row[field].lower())
-                    #combine queries based on boolean 
-                    if spl_query[1] == 'and':
-                        if v1_query and v2_query:
-                            filter_result.append(row)
-                    elif spl_query[1] == 'or':
-                        if v1_query or v2_query:
-                            filter_result.append(row)
-                    elif spl_query[1] == 'not':
-                        if v1_query and not v2_query:
-                            filter_result.append(row)
+                    if re.search(l_query, row[field].lower()):
+                        filter_result.append(row)
                 if len(filter_result) == 0:
-                    return 'There are no results for your query, please try again \U0001F647'
-                else:
-                    return filter_result
+                    print('There are no results for your query, please try again \U0001F647.')
+            # if there are operators in query
+            else:
+                # split the query into tokens
+                spl_query = l_query.split(" ")
+                # check if the query contains more than three tokens or just one token
+                if 2 < len(spl_query) > 3:
+                    print('Your query must follow the following format: "<operator> <tokens>" for comparisons or "<tokens 1> <operator> <tokens 2>" for boolean searches. There appears to be too many tokens \U0001F645.')
+                # if query has two tokens
+                elif len(spl_query) == 2:
+                    # check that first token is a valid operator
+                    if not re.fullmatch(r'<|>|<=|>=|==|!=', spl_query[0]): #or re.fullmatch(r'<|>|<=|>=|==|!=', spl_query[1]):
+                        print('It looks like your query is not written with the following format: "<operator> <tokens>" for comparison \U0001F645.')
+                    else:
+                        # look for first token in dictionary of operators and set it as a variable
+                        co_ops = c_ops[spl_query[0]]
+                        v_query = spl_query[1]
+                        # search for query in data using operator library
+                        for row in data:
+                            if co_ops(row[field].lower(), v_query):
+                                filter_result.append(row)
+                        if len(filter_result) == 0:
+                            print('There are no results for your query, please try again \U0001F647.')
+                # if query has three tokens
+                elif len(spl_query) == 3:
+                    # check that second token is a boolean
+                    if not re.match(r'and|or|not', spl_query[1]):
+                        print('Your query must follow the following format: "<tokens 1> <operator> <tokens 2>." The boolean operator seems to be in the wrong place \U0001F631.')
+                    else:
+                        for row in data:
+                            v1_query = re.search(spl_query[0], row[field].lower())
+                            v2_query = re.search(spl_query[2], row[field].lower())
+                            # combine queries based on boolean
+                            if spl_query[1] == 'and':
+                                if v1_query and v2_query:
+                                    filter_result.append(row)
+                            elif spl_query[1] == 'or':
+                                if v1_query or v2_query:
+                                    filter_result.append(row)
+                            elif spl_query[1] == 'not':
+                                if v1_query and not v2_query:
+                                    filter_result.append(row)
+                        if len(filter_result) == 0:
+                            print('There are no results for your query, please try again \U0001F647.')
+    return filter_result
